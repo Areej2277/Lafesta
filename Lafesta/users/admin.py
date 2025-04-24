906bbea641fda7fe63bc6ba21db6bcf683ddbdd0
Areej <areej.a7676@gmail.com>
2025-04-24 19:10:55 +0300

    Add Profile model to admin panel with custom list display

--- Lafesta/users/admin.py
+++ Lafesta/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'city', 'phone_number', 'date_joined')
 
 # Register your models here.

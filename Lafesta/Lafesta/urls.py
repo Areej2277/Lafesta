6d71dcaeb5c000b6c812811455bdd006ada74e2c
rahaf fallatah <rahaf77553@gmail.com>
2025-04-24 10:17:00 +0300

    The models for the user application and the some  views were created.

--- Lafesta/Lafesta/urls.py
+++ Lafesta/Lafesta/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
+    path('customer/', include('customer.urls'))
     
 ]
 

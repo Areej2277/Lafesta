621bcc1b83b3bfb56e644e688a169c6d29604281
eman rajhi <emyksa1429@gmail.com>
2025-04-30 14:24:32 +0300

    The shipping application has been modified, shipment tracking has been added from the owner and customer, and shipment status has been modified.

--- Lafesta/dresses/admin.py
+++ Lafesta/dresses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Dress,Review,Rental
 # Register your models here.
+admin.site.register(Dress)
+admin.site.register(Review)
+admin.site.register(Rental)
+

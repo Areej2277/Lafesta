125bab31a7a4a2744f73a1c2c3b4a2172f5fba18
Areej <areej.a7676@gmail.com>
2025-05-07 13:50:10 +0300

    modifiying database

--- Lafesta/Lafesta/settings.py
+++ Lafesta/Lafesta/settings.py
@@ -89,12 +89,12 @@ DATABASES = {
         'HOST': os.environ["PGHOST"],
         'PORT': os.environ["PGPORT"],
     }
-} if not DEBUG else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+} # if not DEBUG else {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
